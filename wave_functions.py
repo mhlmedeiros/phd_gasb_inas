0e7863cfa638bd2f70362950067b4444d26b8685
Marcos <mhlmedeiros@gmail.com>
2019-10-07 15:22:32 -0300

    Change name of "gasb_geometry" module
    To avoid confusion with the actual systems' geometry described in the module "shapes".

--- wave_functions.py
+++ wave_functions.py
@@ -1,3 +1,14 @@
+'''
+Script for analysis of wavefunctions on GaSb/InAs/GaSb simmetric quantum wells.
+
+This piece code is part of the project "phd_gasb_inas", which comprises the work
+related to the Phd. Dissertation named: "Quantum transport of charge and spin in
+topological insulators 2D".
+
+Author: Marcos Medeiros
+email: mhlmedeiros@gmail.com
+'''
+
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -6,4 +17,5 @@ import matplotlib.pyplot as plt
 import kwant
 
 # local application imports
-from hamiltonians import 
+from hamiltonians import gasb_hmailtonian as gasb
+from system_geometry import shapes
