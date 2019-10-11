2c11b68b47ca18223f8cccce52d42ef72c843bed
Marcos <mhlmedeiros@gmail.com>
2019-10-10 22:08:56 -0300

    Plot of wave_function section
    Plot in the middle of scattering region.

--- wave_functions.py
+++ wave_functions.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 '''
 Script for analysis of wavefunctions on GaSb/InAs/GaSb simmetric quantum wells.
 
@@ -8,7 +9,6 @@ topological insulators 2D".
 Author: Marcos Medeiros
 email: mhlmedeiros@gmail.com
 '''
-#!/usr/bin/env python
 
 import numpy as np
 import matplotlib
@@ -32,6 +32,26 @@ def plot_density(syst, psi_sqrd):
     plt.show()
     return 0
 
+def density_in_line(syst, states):
+
+    def line(site):
+        (x, y) = site.pos
+        half   = shapes.L_STD/2
+        delta  = shapes.A_STD
+        return abs(x - half) <= delta
+
+    rho_line = kwant.operator.Density(syst, where = line, sum = False)
+    dos_line = np.array([rho_line(p) for p in states])
+    return dos_line
+
+def plot_dos_in_line(dos_line):
+    fig, ax = plt.subplots(1, 2, figsize = (10,5))
+    ax[0].plot(dos_line[0], color = 'red')
+    ax[1].plot(dos_line[1], color = 'blue')
+    plt.tight_layout()
+    plt.show()
+
+
 def main():
     # Define the system:
     hamiltonian = gasb.hamiltonian_103_k_plus()
@@ -45,12 +65,24 @@ def main():
     wf = kwant.wave_function(syst, energy=energia, params=parametros)
     psi = wf(0)
 
-    # Calculate the density:
-    rho = kwant.operator.Density(syst)
-    psi_sqrd = sum(rho(p) for p in psi)
+    # # Calculate the density:
+    # rho = kwant.operator.Density(syst)
+    # psi_sqrd = sum(rho(p) for p in psi)
+    #
+    # # Plot the results:
+    # plot_density(syst, psi_sqrd)
 
-    # Plot the results:
-    plot_density(syst, psi_sqrd)
+    # Calculate dos in a line
+    dos_in_line_from_left  = density_in_line(syst, psi)
+    dos_in_line_from_both  = density_in_line(syst, np.vstack((wf(0),wf(1))))
+    plt.plot(sum(dos_in_line_from_both))
+    plt.show()
+    # print(dos_in_line.shape)
+    # print(dos_in_line)
+    # plot_dos_in_line(dos_in_line_from_left)
+    # plot_dos_in_line(dos_in_line_from_both)
+    print(sum(dos_in_line_from_both).shape)
+
 
 
 if __name__ == '__main__':
