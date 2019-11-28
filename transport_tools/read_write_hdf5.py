932ec2356ddcac3c0bca3b299196f5588fe7cb0d
Marcos <mhlmedeiros@gmail.com>
2019-11-28 17:42:07 -0300

    Create "read_write_hdf5.py"
    This file is meant to be the central manager of saving data process.

---
+++ transport_tools/read_write_hdf5.py
@@ -0,0 +1,23 @@
+import h5py as h5
+
+
+"""
+    groups will be given by the following values:
+     - 97; int
+     -103; int
+     -110; int
+    
+    attributes (?)
+     - Fermi level: mu
+     - Electric field: eF
+     - total current: G (?)
+    
+    data
+     - total local current:
+     - spin_up:
+     - spin_down:
+"""
+
+def write(file_name, group, attr, data):
+    pass
+
